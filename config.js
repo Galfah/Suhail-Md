7397bf33e3ed2b9770c1336e9b1780db9a2b5c9d
Galfah <166868062+Galfah@users.noreply.github.com>
2024-07-31 17:39:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "",
   packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "Suhail",
+  botname : process.env.BOT_NAME  || "Rockie-ᴍᴅ",
+  ownername:process.env.OWNER_NAME|| "Rockie",
 
 
   errorChat : process.env.ERROR_CHAT || "",
